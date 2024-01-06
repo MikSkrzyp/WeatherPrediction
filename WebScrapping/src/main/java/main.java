a0c7c4d3cef9ef4950ffe6548bb7dfe04d7f38c6
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2024-01-07 00:14:15 +0100

    Temperature scrapping part 1

---
+++ WebScrapping/src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        scrapowanie.beforeTest();
+        scrapowanie.ScrapowanieTemperatury();
+
+        scrapowanie.afterTest();
+    }
+}
---
+++ WebScrapping/src/main/java/scrapowanie.java
@@ -0,0 +1,74 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+
+public class scrapowanie {
+
+
+
+    private static WebDriver driver;
+
+
+    public static void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+
+
+    }
+
+
+
+    public static void ScrapowanieTemperatury(){
+        driver.navigate().to("https://www.wunderground.com/history/monthly/us/mi/detroit/KDET");
+        List<WebElement> temperatureElements = driver.findElements(By.xpath("//*[@id='inner-content']/div[2]/div[1]/div[5]/div[1]/div/lib-city-history-observation/div/div[2]/table/tbody/tr/td[2]/table/tr/td[2]"));
+
+        String csv_file_path = "src/main/temperatures.csv";
+
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(csv_file_path));
+
+            // Write the temperatures to the CSV file
+            int i = 1;
+            while (i < temperatureElements.size()) {
+                String temperature = temperatureElements.get(i).getText();
+                writer.write(temperature);
+                writer.newLine();
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public static void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
+
+
+
